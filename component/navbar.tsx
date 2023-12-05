5a0ac196794e3e38ba384c4660e1e84b4520cf8c
mac <mac@macs-MacBook-Air.local>
2023-12-05 16:13:50 +0530

    1st blood

---
+++ component/navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+const Navbar = () => {
+    return (
+        <div className="bg-yellow-600 h-16 flex pl-17px pt-10  items-center w-full">
+            <p className="font-semibold w-full bg-yellow-500 ">ProductInfo Management</p>
+        </div>
+    )
+
+}
+export default Navbar
---
+++ data.js
@@ -0,0 +1,6 @@
+export const productList = [
+  { productId: "1", productName: "vishu", description: "dsfadsfasdf", price: 22 },
+  { productId: "2", productName: "bnx", description: "dsfadsfasdf", price: 22 },
+  { productId: "3", productName: "vidsfshu", description: "dsfadsfasdf", price: 22 },
+  { productId: "4", productName: "vdfvishu", description: "dsfadsfasdf", price: 22 },
+];
